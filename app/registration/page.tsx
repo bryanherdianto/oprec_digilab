6562da0998c27ef112d28ff95192546af2ff2ebf
Bryan Herdianto <bryan.herdianto17@gmail.com>
2025-04-24 07:12:54 +0700

    added login register and sidebar

---
+++ app/registration/page.tsx
@@ -0,0 +1,10 @@
+import { SidebarDemo } from '@/components/SidebarDemo'
+import React from 'react'
+
+function page() {
+  return (
+    <SidebarDemo />
+  )
+}
+
+export default page
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignupForm from '@/components/SignupForm'
+import React from 'react'
+
+function Signup() {
+  return (
+    <SignupForm />
+  )
+}
+
+export default Signup
